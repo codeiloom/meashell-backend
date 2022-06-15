50733bf41e27717f7785271f91787c608c16c386
codeindev <codeindevelopment@gmail.com>
2022-06-15 23:45:33 +0430

    implement auth system

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -3,9 +3,68 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    //
+
+    //  Login user 
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required']
+        ]);
+
+        if ($validate->fails()) {
+
+            return response()->json([
+                'message' => $validate->errors()->first(),
+            ]);
+        } else {
+
+            $cerdinentails = request(['email', 'password']);
+            if (!Auth::attempt($cerdinentails)) {
+                return response()->json([
+                    'message' => 'Unauthorized',
+                ], 401);
+            }
+
+            $user = Auth::user();
+
+            if (!$user->hasVerifiedEmail()) {
+
+                return response()->json([
+                    'message' => 'Email has not verified',
+                ], 401);
+            }
+
+            $tokenResult = $user->createToken('Login Token');
+            $token = $tokenResult->token;
+            $token->save();
+
+            return response()->json([
+                'token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString(),
+
+
+            ], 200);
+        }
+    }
+
+
+    public function users()
+    {
+        $user = User::all();
+        return response()->json([
+            'user' => $user,
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,13 @@ Route::prefix('v1')->group(function () {
     Route::post('register', [RegisterController::class, 'register']);
 
     Route::get('register/activation/{token}', [RegisterController::class, 'signupActive']);
+
+    Route::post('login', [LoginController::class, 'login']);
+
+    // Private Route
+    Route::middleware('auth:api')->group(function(){
+
+        Route::get('users', [LoginController::class, 'users']);
+
+    });
 });
